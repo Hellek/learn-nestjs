b70a964674e09d8d16286e152f2e31b0a58f313c
Roman Basharin <hellekrb@yandex.ru>
2024-08-05 19:53:19 +0300

    Restyle spinner

---
+++ front/src/components/Spinner.tsx
@@ -0,0 +1,3 @@
+export const Spinner = () => (
+  <div className="animate-spin border-8 border-blue-600 border-solid w-12 h-12" />
+)
--- front/src/layout/DefaultLayout.tsx
+++ front/src/layout/DefaultLayout.tsx
@@ -2,11 +2,13 @@ import 'app/theme/styles/tailwind.css'
 
 import { ReactElement } from 'react'
 
+import { Spinner } from '@components/Spinner'
+
 const DefaultLayout = ({ loading, children }: { loading: boolean, children: ReactElement }) => (
   <div className="grow bg-gray-800 text-white p-4 flex flex-col">
     {loading ? (
       <div className="grow flex-center">
-        Spinner
+        <Spinner />
       </div>
     ) : children}
   </div>
